f48e703c69fc6a98a093ba675e9bc949be2a69e3
Hein Htet Aung <heinhtaung@gmail.com>
2024-03-16 09:12:14 +0630

    fix: clean up node_modules and dist (#2)
    * fix: remove node_modules and dist
    
    * fix: update gitignore

--- dist/esm/components/Button/Button.d.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { ButtonProps } from "../../types/button";
-import "./Button.scss";
-declare const Button: (props: ButtonProps) => React.JSX.Element;
-export default Button;
